23fafa27896c7fed774214c458aa15d20fddf4a3
ahiezer <ahiezerrivas@gmail.com>
2022-08-02 21:57:16 -0400

    clase 5

--- Proyecto1/views.py
+++ Proyecto1/views.py
@@ -1,8 +1,20 @@
 import datetime
+
 from django.http import HttpResponse
+from django.template import Template, Context
 
 def saludo(request): #primera vista
-    return HttpResponse("Hola alumnos esta es nuestra primera pagina don django")
+
+    doc_externo=open("C:/Users/ahiezer/Documents/pildoras informaticas/django/Proyecto1/Proyecto1/plantillas/miplantilla.html")
+
+    plt = Template(doc_externo.read())
+
+    doc_externo.close()
+
+    ctx= Context()
+
+    documento=plt.render(ctx)
+    return HttpResponse(documento)
 
 def despedida(request): #primera vista
     return HttpResponse("Hasta luegodjango")
